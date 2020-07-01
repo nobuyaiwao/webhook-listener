240cb2ab55c32d89bec181f5fed490181825e15a
nobuyaiwao <nobuyaiwao@gmail.com>
2020-07-01 10:11:38 +0900

    Accept POST webhook notification

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.post('/listener', function(req, res) {
+    // request body
+    console.log(req.body);
+    // print name
+    console.log(req.body.live);
+    console.log(req.body.notificationItems);
+
+    res.send("[accept]");
+})
+
+//express()
+//  .use(express.static(path.join(__dirname, 'public')))
+//  .set('views', path.join(__dirname, 'views'))
+//  .set('view engine', 'ejs')
+//  .get('/', (req, res) => res.render('pages/index'))
+//  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
